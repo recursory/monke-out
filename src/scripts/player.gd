97c20489d33023716d81ead76ef586ec07ee8be2
dylan grafmyre <dylan.grafmyre@gmail.com>
2023-02-06 22:02:29 -0800

    monke with friggen lazer beams

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -2,26 +2,31 @@ extends KinematicBody2D
 
 
 
-export (int) var speed = 200
+export (int) var speed = 2.0
 
 var velocity = Vector2()
 onready var body = self
 
 func get_input():
-	velocity = Vector2()
+	var jolt = Vector2()
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
+		jolt.x += 1
 	if Input.is_action_pressed("left"):
-		velocity.x -= 1
+		jolt.x -= 1
 	if Input.is_action_pressed("down"):
-		velocity.y += 1
+		jolt.y += 1
 	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+		jolt.y -= 1
+	velocity += jolt.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
-	velocity = body.move_and_slide(velocity)
+	var hittest = body.move_and_collide(velocity)
+	if hittest:
+		velocity *= 0
+		print(hittest)
+	
+	velocity *= 0.9
 	#print(delta, ", ", velocity)
 
 onready var debug_line = $"../debug/Line2D"
@@ -30,21 +35,21 @@ func _ready():
 	#print(debug_line.position, ", ", body.position)
 	pass
 
-onready var walls = $"../walls"
+onready var pillars = $"../pillars"
 func _process(delta):
 	debug_line.clear_points()
 	debug_line.add_point(body.position)
 	
-	for child in walls.get_children():
+	for child in pillars.get_children():
 		var left = Vector2(-child.radius, 0)
 		var right = Vector2(child.radius, 0)
-		var delta2 = body.position - child.position
+		var delta2 = body.position - child.position2
 		var atan3 = atan2(delta2.x, delta2.y)
 		left = left.rotated(-atan3)
 		right = right.rotated(-atan3)
 	
-		debug_line.add_point(child.position + left)
-		debug_line.add_point(child.position + right)
+		debug_line.add_point(child.position2 + left)
+		debug_line.add_point(child.position2 + right)
 		debug_line.add_point(body.position)
 	
 		
